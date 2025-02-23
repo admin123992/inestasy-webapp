2129c8d22ecef9b7762f2f660a943beb8463aaa4
Fabio Nieddu <Fabio@192.168.1.130>
2025-02-23 20:40:58 +0100

    Forzato nuovo deploy su Vercel

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['example.com'], // Se usi immagini esterne, sostituisci example.com
-  },
-  output: 'standalone', // Per abilitare la build ottimizzata per Vercel
-};
-
-module.exports = nextConfig;
